8dbf4efb03bb4ee160b86675e0c6560b44c5b798
weiting.p <w.peng@student.fdu.edu>
2024-10-14 18:27:49 -0700

    change authentication page

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -11,20 +11,19 @@
 
     <div class="row">
       <div class="col-12 justify-content-center d-flex flex-row pt-5 mt-2">
-        <div id="signin-div" class="flex-item border">
+        <div id="signin-div" class="flex-item content-box">
           <h2 class="text-center pt-4 pl-4">Sign In</h2>
-          <form @submit="signin" class="text-center pt-4 pl-4 pr-4">
+          <form @submit="signin" class="text-center pl-4 pr-4">
             <div class="form-group">
-              <label>e-Mail</label>
+              <label class="label-text">E-Mail:</label>
               <input type="email" class="form-control" v-model="email" required />
             </div>
             <div class="form-group">
-              <label>Password</label>
+              <label class="label-text">Password:</label>
               <input type="password" class="form-control" v-model="password" required />
             </div>
-            <small class="form-text text-muted">By continuing, you agree to Simplecoding's Conditions of Use and
-              Privacy Notice.</small>
-            <button type="submit" class="btn btn-primary mt-2 py-0">
+            <!-- <small class="form-text text-muted ty-text">Automobile  Trading Center Group 15</small> -->
+            <button type="submit" class="btn-continue btn btn-primary mt-2 py-0">
               Continue
               <div v-if="loading" class="spinner-border spinner-border-sm" role="status">
                 <span class="sr-only">Loading...</span>
@@ -32,10 +31,16 @@
             </button>
           </form>
           <hr />
-          <small class="form-text text-muted pt-2 pl-4 text-center">New to Simplecoding?</small>
+          <!-- <small class="form-text text-muted pt-2 pl-4 text-center">New to Automobile Trading Center Group 15?</small> -->
           <p class="text-center">
-            <router-link :to="{ name: 'Signup' }" class="btn btn-dark text-center mx-auto px-5 py-1 mb-2">Create Your
-              Simplecoding Account</router-link>
+            <!-- <router-link :to="{ name: 'Signup' }" class="create-btn btn btn-dark text-center px-5 py-1 mb-2">Create Your
+              Automobile Trading
+              <br/>
+              Center Group 15 Account</router-link> -->
+              <router-link :to="{ name: 'Signup' }" class="create-btn btn btn-dark text-center px-5 py-1 mb-2">Create Your
+              Automobile Trading</router-link>
+
+              <!-- wt edit 14-42 line -->
           </p>
         </div>
       </div>
@@ -115,4 +120,39 @@ export default {
     width: 40%;
   }
 }
+
+/* wt add 125-156 */
+.content-box{ 
+  padding: 20px 25px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+.label-text{
+  /* text-align: left !important; */
+  display: flex;
+  justify-content: flex-start;
+  color: #989494;
+  margin-bottom: 10px;
+  margin-top: 15px;
+}
+.ty-text{
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 10px;
+}
+.btn-continue{
+  border-radius: 5px;
+  height: 35px;
+  margin-top: 20px !important;
+}
+.create-btn{
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 15px;
+  width: 320px;
+  text-align: left;
+  border-radius: 5px;
+}
+
 </style>
