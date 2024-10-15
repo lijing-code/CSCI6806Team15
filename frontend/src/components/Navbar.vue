92ec6953f8f64df1d109e275d9bc84a9dfa5aea2
Jing Li <lijing900913@gmail.com>
2024-10-14 19:55:29 -0700

    Update Navbar.vue for search part

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -12,8 +12,8 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!--      Search Bar-->
-      <form class="form-inline ml-auto mr-auto">
+      <!--      Search Bar Old Version-->
+      <!-- <form class="form-inline ml-auto mr-auto">
         <div class="input-group">
           <input size="100" type="text" class="form-control" placeholder="Search Items" aria-label="Username"
             aria-describedby="basic-addon1" />
@@ -27,7 +27,23 @@
             </span>
           </div>
         </div>
-      </form>
+      </form> -->
+
+      <form class="form-inline ml-auto mr-auto" @submit.prevent="searchItems">
+      <div class="input-group">
+        <input size="100" v-model="searchQuery" type="text" class="form-control" placeholder="Search Items" aria-label="Username"
+          aria-describedby="basic-addon1" />
+        <div class="input-group-prepend">
+          <button class="input-group-text" id="search-button-navbar" @click="searchItems">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+              viewBox="0 0 16 16">
+              <path
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </form>
 
       <!--      DropDowns-->
       <ul class="navbar-nav ml-auto">
