f37bb19ab9f3eae6f06d21c36980e72c09d0518b
Jiawen Li <premier.jarvan@gmail.com>
2024-11-11 16:51:08 -0800

    Add Chatbox function and vue file --- Jiawen Li 111124

---
+++ frontend/src/components/Chatbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="chatbox-container"></div>
+</template>
+
+<script>
+export default {
+  name: "Chatbox",
+  mounted() {
+    // Configuring Chatbox Settings --- Jiawen Li 111124
+    window.embeddedChatbotConfig = {
+      chatbotId: "",
+      domain: "www.chatbase.co"
+    };
+
+    // Dynamically inserting scripts for Chatbox --- Jiawen Li 111124
+    const script = document.createElement("script");
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.defer = true;
+    
+    // Checks to see if the script already exists, preventing duplicate loads --- Jiawen Li 111124
+    if (!document.querySelector(`script[src="${script.src}"]`)) {
+      document.body.appendChild(script);
+    }
+  }
+};
+</script>
